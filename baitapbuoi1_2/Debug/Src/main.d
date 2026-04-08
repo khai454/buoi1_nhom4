56edc79cfe6ba14e2f26449d839824b202c77f34
khai454 <tranquangkhai454@gmail.com>
2026-04-08 10:11:39 +0700

    first commit

---
+++ baitapbuoi1_2/Debug/Src/main.d
@@ -0,0 +1,10 @@
+Src/main.o: ../Src/main.c \
+ C:/Users/PC/Desktop/bai\ bao\ cao\ ben\ mindx/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/rtos/timer.h \
+ C:/Users/PC/Desktop/bai\ bao\ cao\ ben\ mindx/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/ucglib/Ucglib.h \
+ C:/Users/PC/Desktop/bai\ bao\ cao\ ben\ mindx/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/ucglib/ucg.h
+
+C:/Users/PC/Desktop/bai\ bao\ cao\ ben\ mindx/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/rtos/timer.h:
+
+C:/Users/PC/Desktop/bai\ bao\ cao\ ben\ mindx/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/ucglib/Ucglib.h:
+
+C:/Users/PC/Desktop/bai\ bao\ cao\ ben\ mindx/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/ucglib/ucg.h:
